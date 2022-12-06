092d8cc8d41d4236b5c0970616c81713338abfa0
jespereriksson92 <erjesper@gmail.com>
2022-12-06 10:56:31 +0100

    Added insert function

--- assess1.py
+++ assess1.py
@@ -18,6 +18,18 @@ def view_contacts():
     cursor.close()
     connection.close()
     print(view)
+#3.6
+def add_contact(first_name, last_name, title, organization):
+    connection = get_db_connection()
+    cursor = connection.cursor()
+    insert_query = (f"INSERT INTO contacts (first_name, last_name, title, organization)VALUES ('{first_name}', '{last_name}', '{title}', '{organization}');")
+    cursor.execute(insert_query)
+    connection.commit()
+    cursor.close()
+    connection.close()
+    print(f"{first_name} added!")
+
+#3.5
 print("Welcome! What would you like to do?")
 while True:
     user_input = input("\nLIST \t See an overview of the contacts\n"
@@ -26,7 +38,11 @@ while True:
     if user_input == 'LIST':
         view_contacts()
     elif user_input == 'INSERT':
-        print()
+        first_name_input = input("What is the first name of the person? ")
+        last_name_input = input("What is the last name of the person? ")
+        title_input = input("Give a title to this person, e.g. brother, mortal enemy? ")
+        organization_input = input("Does this person belong to a certain organization? ")
+        add_contact(first_name_input,last_name_input,title_input,organization_input)
     elif user_input == 'DELETE':
         print()
     elif user_input == 'QUIT':
